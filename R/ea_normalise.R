17958074d6a56c72ec2920de3daa77539a9e7536
Anders Kolstad <anders.kolstad@nina.no>
2022-12-21 10:53:35 +0100

    fix: rlang dependency. CMD pass

--- R/ea_normalise.R
+++ R/ea_normalise.R
@@ -2,26 +2,25 @@
 #'
 #' A function to normalise, or re-scale, a numerical vector such as a condition variable to become a value between 0 and 1, bound by an upper reference level.
 #'
-#' @param data data set of class sf
+#' @param data Data set of class sf
 #' @param vector Numerical vector inside `data` which should be normalised
 #' @param upper_reference_level The upper reference level against which to normalise the `vector`. Can be a single number or a vector of length equal to `vector`.
 #' @param lower_reference_level The lower reference level against which to normalise the `vector`. Defaults to 0. Can be a single number or a vector of length equal to `vector`.
-#' @param scaling_function one of c("linear", "exponential)
+#' @param scaling_function one of c("linear", "sigmoid", "exponential convex", "exponential concave")
+#' @param reverse Logical. Should the indicator values be reversed (i.e. giving a high variable value a low indicator value)
+#' @param break_point Numerical vector or single value indicating the value of the variable which should be scaled to 0.6 in the indicator.
+#' @param optimum Numerical vector or single value indicating the upper reference value for a two-sided indicator.
 #'
 #' @return numerical vector, a normalised version of `x`
+#'
+#' @importFrom rlang is_empty
 #' @export
 #'
 #' @examples
-#' #get example data (an sf object)
 #' data("ex_polygons")
-#' hist(ex_polygons$condition_variable_1,
-#'      main="", xlab="Condition variable 1")
-#'
-#' # Normalise using static upper reference value
 #' hist(ea_normalise(data = ex_polygons,
-#'                   vector = "condition_variable_1",
-#'                   upper_reference_level = 10),
-#'      main="", xlab="Indicator value")
+#'             vector = "condition_variable_2",
+#'             upper_reference_level = 8))
  ea_normalise <- function(data = NULL,
                           vector = NULL,
                           upper_reference_level = NULL,
