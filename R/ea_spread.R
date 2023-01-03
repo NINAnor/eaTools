366d04c8921d503da8427f932b044e23a818e31f
Anders Kolstad <anders.kolstad@nina.no>
2023-01-03 16:22:45 +0100

    feat: Start ea_spread

---
+++ R/ea_spread.R
@@ -0,0 +1,16 @@
+
+
+
+
+data("ex_polygons")
+data("accounting_area")
+
+indicator <- ex_polygons
+regions <- accounting_area
+
+ea_spread <- function(indicator = NULL,
+                      regions = NULL,
+                      threshold = 1){
+
+
+}
