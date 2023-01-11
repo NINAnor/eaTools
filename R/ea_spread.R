3198e49b8000f6e343a823cb8666db7a484aeb0f
Anders Kolstad <anders.kolstad@nina.no>
2023-01-11 13:04:30 +0100

    feat: ea_spread works

--- R/ea_spread.R
+++ R/ea_spread.R
@@ -4,30 +4,42 @@
 
 
 
-ea_spread <- function(indicator_data = NULL,
-                      indicator = NULL,
-                      regions = NULL,
-                      group = "values",
+ea_spread <- function(indicator_data,
+                      indicator,
+                      regions,
+                      groups,
                       threshold = 1){
   if("sf" %in% class(indicator_data) & "sf" %in% class(regions)){
-    # get the intersections
-  indicator_split <- sf::st_intersection(indicator_data, regions)
-    # calculate area of the intersections
-  indicator_split$area <- units::drop_units(sf::st_area(indicator_split))
+      # get the intersections
+    st_agr(indicator_data) <- "constant"
+    st_agr(regions) <- "constant"
+    indicator_split <- sf::st_intersection(indicator_data, regions)
+      # calculate area of the intersections
+    indicator_split$area <- units::drop_units(sf::st_area(indicator_split))
   } else stop("The input data is not in a supported format. Both indicators and regions need to be sf objects.")
-    # Calculate the area weighted mean indicator values for each region
-  if(is_empty(indicator)) stop("Indicator column is not defined")
-  if(is_empty(group)) stop("Group is not defined")
-  myWeightedMeans <- indicator_split %>%
-      group_by(group) %>%
+      # Calculate the area weighted mean indicator values for each region
+  if(missing(indicator)) stop("Indicator column is not defined")
+  if(missing(groups)) stop("Group is not defined")
+
+  groups_int <- enquo(groups)
+  indicator_int <- enquo(indicator)
+
+
+  myWeightedMeans <<- indicator_split %>%
+      group_by(!!groups_int) %>%
       mutate(n = n(),
-             indicator_NA = ifelse(n >=threshold, indicator, NA)) %>%
+             indicator_NA = ifelse(n >=threshold, !!indicator_int, NA))%>%
       summarise(meanIndicatorValue =
-                  weighted.mean(x = indicator_NA,
-                                w = area))
-    # paste these new values into the regions dataset
-  regions$meanIndicator <- myWeightedMeans$meanIndicatorValue[match(regions$group, myWeightedMeans$group)]
-  return(regions)
+                  stats::weighted.mean(x = indicator_NA,
+                                       w = area)) %>%
+      mutate("ID" = !!groups_int) %>%
+      as.data.frame() %>%
+      select(-SHAPE)
 
-
+    # paste these new values into the regions data set
+  regions %>%
+    mutate("ID" = !!groups_int) %>%
+    left_join(myWeightedMeans, by = "ID") %>%
+    select(ID, meanIndicatorValue)
 }
+
