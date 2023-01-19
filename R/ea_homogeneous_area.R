b607eadc939d6bb0762dc393278cf4bfd4ffa328
Anders Kolstad <anders.kolstad@nina.no>
2023-01-19 14:54:52 +0100

    feat: Add optional outline delineation to ea_homogeneous_area

--- R/ea_homogeneous_area.R
+++ R/ea_homogeneous_area.R
@@ -5,6 +5,7 @@
 #'
 #' @param delineation1 A geometry (sf or stars object)
 #' @param delineation2 A second, optional, sf object, with the same crs as `delineation1`.
+#' @param outline An optional sf object to mask out areas outside it.
 #' @param keep1 Column names in delineation1 to keep in the returned object. Only if delineation1 is an sf object.
 #' @param keep2 Column names in delineation2 to keep in the returned object.
 #'
@@ -37,28 +38,45 @@
 #'  tmap::tm_polygons(col = "values")+
 #' tmap::tm_shape(ex2[ex2$name == "Enebakk",])+
 #'    tmap::tm_polygons(col = "blue", border.col = "black")
+#' # Example 3: Example 2 + outline
+#' enebakk <- accounting_area[accounting_area$name == "Enebakk",]
+#' ex3 <- ea_homogeneous_area_dev(ex_raster,
+#'           accounting_area,
+#'           enebakk,
+#'           keep2 = "name")
+#' tm_shape(ex3)+
+#'           tm_polygons(col = "values")
 ea_homogeneous_area <- function(delineation1,
                                 delineation2,
+                                outline,
                                 keep1 = NULL,
                                 keep2 = NULL){
   if("stars" %in% class(delineation1)){
     if(max(dim(delineation1)) > 10000) warning("You have inputted a large raster file. Vectorisation might take a long time.")
-    myArea <- sf::st_as_sf(delineation1, as_points = FALSE, merge = TRUE)
+    HIA <- sf::st_as_sf(delineation1, as_points = FALSE, merge = TRUE)
   }
   if("sf" %in% class(delineation1)){
-    myArea <- delineation1 %>%
+    HIA <- delineation1 %>%
       select(all_of(keep1))
   }
   if(missing(delineation2)) {
-    return(myArea)
+    return(HIA)
   } else{
     if("sf" %in% class(delineation2)){
-      myArea <- delineation2 %>%
+      HIA <- delineation2 %>%
         select(all_of(keep2)) %>%
-        st_intersection(myArea) %>%
+        st_intersection(HIA) %>%
         st_collection_extract() %>%
         st_cast(to = "POLYGON")
-      return(myArea)
-    } else stop("Delineation2 is in a format which is not supported.")
+      if(missing(outline)){
+        return(HIA)
+      } else {
+        if("sf" %in% class(outline)){
+          HIA_outline <- HIA %>%
+            st_intersection(outline)
+          return(HIA_outline)
+        } else stop("Outline is not an sf object")
+      }
+    } else stop("Delineation2 is not an sf object")
   }
 }
